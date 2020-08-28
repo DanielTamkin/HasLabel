8678cce4a8c631e95a6736411a86123fdd654b59
DanielTamkin <9532762+DanielTamkin@users.noreply.github.com>
2020-08-28 17:54:18 -0500

    add action events
    - labeled.js : if a label is added or previously added
    - unlabeled.js : if a label is removed

---
+++ src/events/labeled.js
@@ -0,0 +1,45 @@
+const { contains, exact } = require('../util.js');
+
+/**
+ * Executed when a pull_request or synchronize event is triggerd.
+ *
+ * @method labeled
+ * @param  {String} label Pre-determined by main action file.
+ * @param  {Object} tools Toolkit tools,
+ * @return {1 or 0}       Returned by .fail or .success
+ */
+module.exports = async (label, tools) => {
+  const { context } = tools;
+  let result = false;
+
+  if(tools.inputs.contains) {
+
+    if (contains(label, context.payload)) {
+      tools.outputs[`labeled-${label}`] = true;
+      result = true;
+    }
+
+    await tools.exit.success(`${
+      (result)
+        ? '[CONTAINS] label '+label
+        : 'label not added'
+    }`);
+
+
+  } else if (tools.inputs.exact) {
+
+    if (exact(label, context.payload.label)) {
+      tools.outputs[`labeled-${label}`] = true;
+      result = true;
+    }
+
+    await tools.exit.success(`${
+      (result)
+        ? '[EXACT] Specified label added'
+        : 'Specified label not added'
+    }`);
+
+  } else {
+    await tools.exit.fail('You must specify a label.');
+  }
+}
---
+++ src/events/unlabeled.js
@@ -0,0 +1,44 @@
+const { contains, exact } = require('../util.js');
+
+module.exports = async (label, tools) => {
+  const { context } = tools;
+  let result = false;
+
+  if(tools.inputs.contains) {
+    if (contains(label, context.payload)) {
+      tools.outputs[`unlabeled-${label}`] = true;
+      result = true;
+    }
+
+    await tools.exit.success(`${
+      (result)
+        ? '[CONTAINS] '+label+' unlabeled'
+        : label+' not unlabeled'
+    }`);
+
+  } else if (tools.inputs.exact) {
+    if (exact(label, context.payload.label)) {
+      tools.outputs[`unlabeled-${label}`] = true;
+      result = true;
+    }
+
+    await tools.exit.success(`${
+      (result)
+        ? '[EXACT] '+label+' unlabeled'
+        :   label+' not unlabeled'
+    }`);
+
+  } else {
+    await tools.exit.fail('You must specify a label.');
+  }
+}
+
+// if(tools.inputs.contains) {
+//   const label = tools.inputs.contains;
+//   let result = false;
+//
+//   if (!contains(label, context.payload)) {
+//     tools.outputs[`unlabeled-${label}`] = true;
+//     result = true;
+//   }
+// } else
